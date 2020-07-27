cb79fc4a583a07fee01b9e729bd2e2671c90ba01
Rose Kairu <rosekairu@gmail.com>
2020-07-27 16:13:10 +0300

    working on profile update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-// routes
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {ProfileComponent} from './dashboard/profile/profile.component';
+import {UpdateProfileComponent} from './dashboard/update-profile/update-profile.component';
+
+
 const appRoutes: Routes = [
     {path: '', pathMatch: 'full', redirectTo: '/landing-page'},
     { path: 'dashboard', pathMatch: 'full', redirectTo: '/dashboard' },
-    {path: '', pathMatch: 'full', redirectTo: '/create-department'}
+    {path: '', pathMatch: 'full', redirectTo: '/create-department'}, 
+    { path: "profile", component: ProfileComponent },
+    { path: "edit", component: UpdateProfileComponent },
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { RegisterModule } from './register/register.module';
 import { RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
@@ -10,18 +11,24 @@ import {DepartmentModule} from './department/department.module';
 import {ManagerModule} from './manager/manager.module';
 import {LandingModule} from './landing/landing.module';
 import {ReporteeModule} from './reportee/reportee.module'
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './dashboard/profile/profile.component';
+import { ProfileService } from './services/profile/profile.service';
+import { UpdateProfileComponent } from './dashboard/update-profile/update-profile.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    ProfileComponent,
+    UpdateProfileComponent,
   ],
   
   imports: [
     BrowserModule,
     RegisterModule,
-    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
     DepartmentModule,
@@ -29,10 +36,14 @@ import {ReporteeModule} from './reportee/reportee.module'
     LandingModule,
     ReporteeModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule,
     RouterModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
