239d66ee57ea1f778ae8473f01543efadba70578
Rose Kairu <rosekairu@gmail.com>
2020-07-29 11:27:26 +0300

    feature(sidebar):toggle between links
    -sidebar component
    -successfully toggle between links
    -succeful routing between components
    
    []

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatInputModule } from  '@angular/material/input'
 import { MatRadioModule } from  '@angular/material/radio'
@@ -7,21 +8,22 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { FileUploadModule } from 'ng2-file-upload';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ProfileComponent } from './dashboard/profile/profile.component';
 import { ProfileService } from './services/profile/profile.service';
+import { SettingsService } from './services/settings.services';
 import { UpdateProfileComponent } from './dashboard/update-profile/update-profile.component';
+import { SidebarComponent } from "./sidebar/sidebar.component";
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    ProfileComponent,
     UpdateProfileComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
@@ -35,9 +37,10 @@ import { UpdateProfileComponent } from './dashboard/update-profile/update-profil
     MatInputModule,
     BrowserAnimationsModule,
     HttpClientModule, 
-    FileUploadModule
+    FileUploadModule,
+    MatSidenavModule
   ],
-  providers: [ProfileService],
+  providers: [ProfileService, SettingsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/sidebar/sidebar-routes.config.ts
@@ -0,0 +1,9 @@
+export const ROUTES = [
+    { path: 'pending', title: 'Pending Assessments', icon: 'edit', children: null },
+    { path: '/dashboard', title: 'Dashboard', icon: 'dashboard', children: null },
+    { path: 'profile', title: 'User Profile', icon: 'person', children: null },
+    { path: 'assessment', title: 'Assessments', icon: 'edit', children: null },
+    { path: 'reportee', title: 'Reportees', icon: 'person', children: null },
+    { path: 'period', title: 'Assessment Period', icon: 'date_range', children: null },
+    
+];
